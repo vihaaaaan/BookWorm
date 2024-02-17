31c1f6321614eaf3f4b4810078894cd70da4f904
vihaaaaan <vihaanv14@gmail.com>
2024-02-17 14:06:53 -0500

    put static page on app

--- TeacherWorm/views/add_article.py
+++ TeacherWorm/views/add_article.py
@@ -1,4 +1,15 @@
 from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.template import loader
+
 
 def add_article(request):
-    return HttpResponse("add article")
+    context = {}
+    if request.method == 'POST':
+        article_url = request.POST.get('content', None)
+        if article_url:
+            print("Successfully posted:", article_url)
+            #Call a function to get scraped data from URL
+            #Call another function to load scraped data into database model
+
+    return render(request, 'TeacherWorm/article _link_page.html', context)
